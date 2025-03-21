714087afba52d7552df43b57432a3a287e5c06dd
Gane-sc <gane.sc.bsinfotech@gmail.com>
2025-03-21 03:12:06 -0700

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,37 @@
-import React from "react";
-import CourseTable from "./components/CourseTable";
+import React from 'react';
 
-function App() {
+const scheduleData = [
+  { subject: "Integrative Programming and Technologies LEC", day: "Tuesday", time: "8:00 AM - 10:00 AM", room: "LAB C" },
+  { subject: "Networking LEC", day: "Tuesday", time: "03:00 PM - 05:00 PM", room: "LAB A" },
+  { subject: "Web Development LEC", day: "Tuesday", time: "11:00 AM - 1:00 PM", room: "LAB B" },
+  { subject: "Database Management System LEC", day: "Tuesday", time: "6:00 PM - 8:00 PM", room: "LAB D" },
+  { subject: "Introduction to Human Computer Interaction LEC", day: "Tuesday", time: "1:00 PM - 3:00 PM", room: "LAB E" }
+];
+
+const App = () => {
   return (
     <div>
-      <h1>My Course List</h1>
-      <CourseTable />
+      <h2>Schedule</h2>
+      <table style={{ width: "100%", borderCollapse: "collapse", marginBottom: "20px" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Subject</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Day</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Time</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Room</th>
+          </tr>
+        </thead>
+        <tbody>
+          {scheduleData.map((item, index) => (
+            <tr key={index}>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{item.subject}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{item.day}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{item.time}</td>
+              <td style={{ border: "1px solid black", padding: "8px" }}>{item.room}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
